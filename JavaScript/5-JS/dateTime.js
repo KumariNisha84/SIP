0c3f7ca58ffda6a0b27be97c3914adaa2c5566e2
Nisha Kumari <n7053157@gmail.com>
2026-06-23 11:55:34 +0530

    JS - Synchronous v/s Asynchronous, Closure(), datTime(), SetInterval(), setTimeout()

--- JavaScript/5-JS/dateTime.js
+++ JavaScript/5-JS/dateTime.js
@@ -1,10 +1,10 @@
-const time = new Date();
+const now = new Date();
 // console.log(time);
 // console.log(time.getFullYear());
 // console.log(time.getMonth());
 // console.log(time.getDate());
 // console.log(time.getDay());
 
-console.log(time.toDateString());
-console.log(time.toLocaleDateString());
-console.log(time.toLocaleTimeString());
+console.log(now.toDateString());
+console.log(now.toLocaleDateString());
+console.log(now.toLocaleTimeString());
