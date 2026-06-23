7ba8c148b1cfe3cff04dbd73c63c7e9f07c6cd3b
Nisha Kumari <n7053157@gmail.com>
2026-06-23 12:03:39 +0530

    JS - Callback Functions, Methods - filter(), hoisting(), map(), object(), reduce() & Assignments , practice questions

--- JavaScript/4-JS/filter.js
+++ JavaScript/4-JS/filter.js
@@ -1,6 +1,6 @@
 //filter() method
 //syntax - array.filter(callback);
 
-const ages = [10, 20, 30];
+const ages = [10, 20, 30, 15];
 const adults = ages.filter (age => age>=18);
 console.log(adults);
