70b0ade343dac9431697340a627fb525f912ee65
Nisha Kumari <n7053157@gmail.com>
2026-07-13 09:49:48 +0530

    Routes, Controllers

---
+++ EXPRESS/To_Do_List/1_app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const PORT = 3002;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+const todosRoutes = require('./routes/todosRoutes');
+
+app.use('/', todosRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`)
+})
