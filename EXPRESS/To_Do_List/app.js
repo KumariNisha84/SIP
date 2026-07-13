ea9c6e52627c4199b2a8b05e369f3ff3b42b1ffa
Nisha Kumari <n7053157@gmail.com>
2026-07-13 09:24:17 +0530

    Todos with Backend

--- EXPRESS/To_Do_List/app.js
+++ EXPRESS/To_Do_List/app.js
@@ -1,20 +1,66 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = 3002;
-
+const path = require('path')
+const PORT = 3002;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.get('/addTask',(req,res)=>{
-    res.send(`TASK: ${req.query.inpt}`);
+let todos = ["dancing", "Drinking"];
+
+app.get('/addtask', (req, res) => {
+    let { work } = req.query;
+    console.log(work);
+    if (work != "") {
+        todos.push(work);
+    }
+    res.send(todos);
+    // res.redirect('/todos')
 })
 
-app.post('/addTask',(req,res)=>{
-    res.send(`Task: ${req.body.inpt}`)
+app.get('/todos', (req, res) => {
+    res.send(todos);
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port${port}`);
+
+app.get('/increase', (req, res) => {
+    let {work} = req.query;
+    console.log(work);
+    let idx = todos.indexOf(work);
+    console.log(idx);
+    if(idx > 0 && idx < todos.length) {
+        let temp = todos[idx];
+        todos[idx] = todos[idx-1];
+        todos[idx-1] = temp;
+    }
+    res.send(todos);
+})
+
+app.get('/decrease', (req, res) => {
+    let {work} = req.query;
+    console.log(work);
+    let idx = todos.indexOf(work);
+    console.log(idx);
+    if(idx >= 0 && idx < todos.length - 1) {
+        let temp = todos[idx];
+        todos[idx] = todos[idx+1];
+        todos[idx+1] = temp;
+    }
+    res.send(todos);
+})
+
+
+app.get('/delete', (req, res) => {
+    let {work} = req.query;
+    console.log(work);
+    let idx = todos.indexOf(work);
+    console.log(idx);
+    todos.splice(idx, 1);
+    res.send(todos);
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${PORT}`)
 })
